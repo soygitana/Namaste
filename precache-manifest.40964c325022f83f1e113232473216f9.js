29c4a5561435388216d1b0652ff50cd50620724b
Lizon <Paula.Lizon@lionbridge.com>
2020-02-21 11:28:30 +0100

    Updates

---
+++ precache-manifest.40964c325022f83f1e113232473216f9.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247a208997de888ce1f8a226b8315ced",
+    "url": "/namaste/index.html"
+  },
+  {
+    "revision": "58f9fe3abb09c07bc186",
+    "url": "/namaste/static/css/main.b4e197cd.chunk.css"
+  },
+  {
+    "revision": "59db292539ddc9e2cbc1",
+    "url": "/namaste/static/js/2.9a1b1394.chunk.js"
+  },
+  {
+    "revision": "58f9fe3abb09c07bc186",
+    "url": "/namaste/static/js/main.17c02b7a.chunk.js"
+  },
+  {
+    "revision": "b4685826d44bf049c87c",
+    "url": "/namaste/static/js/runtime-main.c872f432.js"
+  },
+  {
+    "revision": "d0d9be2938f69168b71f193837474782",
+    "url": "/namaste/static/media/acro.d0d9be29.jpg"
+  },
+  {
+    "revision": "9304b7da68cbb4ddc272dd9cc8e980cd",
+    "url": "/namaste/static/media/aerial_flow.9304b7da.mp4"
+  },
+  {
+    "revision": "03898af640ac2ef95468ddf4da6b0ade",
+    "url": "/namaste/static/media/aero1.03898af6.jpg"
+  },
+  {
+    "revision": "b644e341340b3bb91e5bc1c544582ddf",
+    "url": "/namaste/static/media/aero2.b644e341.jpg"
+  },
+  {
+    "revision": "475cf39147056532d368eb2b613aa8a7",
+    "url": "/namaste/static/media/aero3.475cf391.jpg"
+  },
+  {
+    "revision": "a1c97db4fcfb21a774c669d02e2b9f93",
+    "url": "/namaste/static/media/background.a1c97db4.JPG"
+  },
+  {
+    "revision": "e975b83ad6344fbe1fcad7945a7b1101",
+    "url": "/namaste/static/media/card1.e975b83a.jpg"
+  },
+  {
+    "revision": "5b985465d7961815daa72de835dc9f7f",
+    "url": "/namaste/static/media/card2.5b985465.jpg"
+  },
+  {
+    "revision": "763ae8c2610d99df49763cadf973d5a3",
+    "url": "/namaste/static/media/card3.763ae8c2.jpg"
+  },
+  {
+    "revision": "a002f9339a671a88e8196c23f3c4dd60",
+    "url": "/namaste/static/media/card4.a002f933.jpg"
+  },
+  {
+    "revision": "a9a950684b52d0b6c196834b0fd66db3",
+    "url": "/namaste/static/media/classic1.a9a95068.jpg"
+  },
+  {
+    "revision": "2186b5ec8199354553f4ecbd7562553a",
+    "url": "/namaste/static/media/dynamic.2186b5ec.jpg"
+  },
+  {
+    "revision": "6371b132dcc68764c642da5c26179164",
+    "url": "/namaste/static/media/hatha.6371b132.jpg"
+  },
+  {
+    "revision": "7cf61e7b0413a04658d25fb54667b036",
+    "url": "/namaste/static/media/home1.7cf61e7b.jpg"
+  },
+  {
+    "revision": "24982e57ed6fe91239d3313ee9801663",
+    "url": "/namaste/static/media/home2.24982e57.jpg"
+  },
+  {
+    "revision": "de6900059768b747d6c8c89f0aa32077",
+    "url": "/namaste/static/media/kids.de690005.jpg"
+  },
+  {
+    "revision": "d83d898394ddb941cf69a5aa542bf5a7",
+    "url": "/namaste/static/media/logo.d83d8983.png"
+  },
+  {
+    "revision": "f5e24d248024c81c25c1337dea826e34",
+    "url": "/namaste/static/media/meditation.f5e24d24.jpg"
+  },
+  {
+    "revision": "04e2d8ff5ee261e49f19bf08506dbcae",
+    "url": "/namaste/static/media/strala.04e2d8ff.jpg"
+  },
+  {
+    "revision": "48e838416ff1d3c2206347d0bfa9a967",
+    "url": "/namaste/static/media/yin.48e83841.jpg"
+  },
+  {
+    "revision": "c7257c0ced4083d46ca8f35c6e33c2d9",
+    "url": "/namaste/static/media/yoga1.c7257c0c.jpg"
+  },
+  {
+    "revision": "dbc9be95c48aec9f5b09d101d602a9a4",
+    "url": "/namaste/static/media/yoga2.dbc9be95.jpg"
+  },
+  {
+    "revision": "5411787d171d4906cbd451a0aaf420d0",
+    "url": "/namaste/static/media/yoga3.5411787d.jpg"
+  },
+  {
+    "revision": "bfb212a1d7ec679b790f16930889506f",
+    "url": "/namaste/static/media/yoga4.bfb212a1.jpg"
+  }
+]);
